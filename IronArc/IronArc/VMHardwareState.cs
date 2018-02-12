2ba2ef3373d3cfeb74790d78102cea8c05cc3745
Chris Akridge <cakridge2@gmail.com>
2018-02-11 21:00:31 -0500

    Add another sample

---
+++ IronArc/IronArc/VMHardwareState.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IronArc
+{
+	public class VMHardwareState
+	{
+		private List<HardwareDevice> devices;
+
+		public IReadOnlyList<HardwareDevice> Devices
+		{
+			get { return devices; }
+		}
+
+		public void AddDevice(HardwareDevice device)
+		{
+			devices.Add(device);
+		}
+
+		public void RemoveDevice(ushort deviceID)
+		{
+			var device = devices.Where(d => d.DeviceID == deviceID).FirstOrDefault();
+
+			if (device == null)
+			{
+				throw new ArgumentException(string.Format("No hardware device has ID #{0}.", deviceID), "deviceID");
+			}
+
+			devices.Remove(device);
+		}
+	}
+}
